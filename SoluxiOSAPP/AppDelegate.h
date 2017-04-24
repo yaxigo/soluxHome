a3b8fb81457eb80f4a5cf637a7c482286108b3de
yaxigo <zh626791701@outlook.com>
2017-04-24 19:47:46 +0800

    Initial Commit

---
+++ SoluxiOSAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SoluxiOSAPP
+//
+//  Created by ZhangHao on 2017/4/24.
+//  Copyright © 2017年 ZhangHao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SoluxiOSAPP/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SoluxiOSAPP
+//
+//  Created by ZhangHao on 2017/4/24.
+//  Copyright © 2017年 ZhangHao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
