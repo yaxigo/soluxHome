0c83beb1b82d36afca822b46861fb47d84ffc8a0
yaxigo <zh626791701@outlook.com>
2017-04-24 20:17:19 +0800

    creat BaseViewconroller

--- SoluxiOSAPP/AppDelegate.m
+++ SoluxiOSAPP/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    //say something
+    //say somethingsda sdasdasdasdasddas
     return YES;
 }
 
---
+++ SoluxiOSAPP/BaseClass/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  SoluxiOSAPP
+//
+//  Created by ZhangHao on 2017/4/24.
+//  Copyright © 2017年 ZhangHao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
